6f9c08d47e695e0896f4f1e3e0953bfa4b8f8d43
Gafera <51031854+Gafera@users.noreply.github.com>
2020-04-06 09:57:29 -0300

    Update analise.py

--- analise.py
+++ analise.py
@@ -1,17 +1,15 @@
 import pandas as pd
-
-def analisa(entrada, saida, quantidade):
-    url1 = 'C:/Users/Gabriel de Freitas/Documents/analises/repositorios/'
-    caminhoFinal = 'C:/Users/Gabriel de Freitas/Downloads/'    
+def analisa(entrada, saida, quantidade):2  
     for j in range(0, quantidade):
-        url1 = url1 + entrada[j]
-        caminhoFinal = caminhoFinal + saida[j]
+        url1 = 'C:/Users/Gabriel de Freitas/Documents/analises/repositorios/'
+        caminhoFinal = 'C:/Users/Gabriel de Freitas/Documents/analises/results/'
+        url1 += entrada[j]
+        caminhoFinal += saida[j]
         arquivo1 = pd.read_excel(url1)
         data = arquivo1['CVE'].value_counts()
         escrever = pd.ExcelWriter(caminhoFinal)
         data.to_excel(escrever, 'aba', index=True)
         escrever.save()
-    
 saida =[]
 entrada = []
 quantidade = int(input("Digite quantas arquivos serão analisados: "))
